242115c66c9b383bddac759c57adb73a693799a8
William F. Cipriano <williamfcipriano@gmail.com>
2016-01-12 07:50:02 -0500

    Improved speed.

--- emailer.py
+++ emailer.py
@@ -7,6 +7,7 @@ def email(server,port,username,password,toaddrs,message):
     server.starttls()
     server.login(username,password)
     #Header needed for T-Mobile
-    header = "From: %s\r\nTo: %s\r\nSubject: %s\r\nX-Mailer: My-Mail\r\n\r\n" % (username, toaddrs, 'subject')
-    server.sendmail(username, toaddrs, header+message)
+    for addr in toaddrs:
+        header = "From: %s\r\nTo: %s\r\nSubject: %s\r\nX-Mailer: FreeMMS\r\n\r\n" % (username, toaddrs, 'subject')
+        server.sendmail(username, toaddrs, header+message)
     server.quit()
